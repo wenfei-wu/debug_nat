8ae4f4a7182d4580371f42b670f3ec54d4320b64
Wenfei Wu <wenfeiwu@outlook.com>
2020-03-03 22:27:33 +0800

    sender

--- sender.py
+++ sender.py
@@ -1,14 +1,14 @@
 from scapy.all import *
 
-PCAPFILE=""
+PCAPFILE="ten.pcap"
 
 f=rdpcap(PCAPFILE)
-idx = 1
+idx = 0
 
-cnt=100
-while cnt>0:
-    sendp(f[idx])
-    cnt-=1
+#cnt=100
+#while cnt>0:
+#    sendp(f[idx])
+#    cnt-=1
 
 f[idx].show()
-f[idx].dumppdf("pkt_"+str(idx)+".pdf")
+f[idx].pdfdump("pkt_"+str(idx)+".pdf")
