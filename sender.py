28ceeb5e2db46bc166199cb18445880b9116607d
Wenfei Wu <wenfeiwu@outlook.com>
2020-03-03 23:31:40 +0800

    pad trace

--- sender.py
+++ sender.py
@@ -1,26 +1,37 @@
 from scapy.all import *
 
-PCAPFILE="ten.pcap"
-IFACE="p9p1"
-IDX=4
+PCAPFILE="univ1_pt1.pcap"
+#IFACE="p9p1"
+#IDX=0
 
-#sendp(IP(ttl=10), iface=IFACE)
 
 
 f=rdpcap(PCAPFILE)
+pkts = [pkt if IP not in pkt else pkt/('a'*max(0, pkt[IP].len-len(pkt[IP]))) for pkt in f]
+wrpcap("univ1_pt1_complete.pcap", pkts)
 
-pkt = f[IDX]
+#pkt = f[IDX]
+#pkt.show()
+#print(len(pkt))
+#print(len(pkt[IP]))
+#print(pkt[IP].len)
+#print(len(pkt[TCP]))
+#pkt = pkt/('a'* (pkt[IP].len-len(pkt[IP])  ))
+#print(pkt)
+#print(len(pkt))
+#print(pkt.time)
 
-new_pkt =
 
+
+#wrpcap("pkt_0_aug.pcap", (pkt))
 
 #for pkt in f:
 #    pkt.show()
 
-cnt=100
-while cnt>0:
-    sendp(pkt, iface=IFACE)
-    cnt-=1
+#cnt=100
+#while cnt>0:
+#    sendp(pkt, iface=IFACE)
+#    cnt-=1
 
 #f[idx].show()
-#f[idx].dumppdf("pkt_"+str(idx)+".pdf")
+#f[idx].pdfdump("pkt_"+str(idx)+".pdf")
