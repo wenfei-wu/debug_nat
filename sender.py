315f70d3850c23f451b49eea43c7c5805d03f354
Wenfei Wu <wenfeiwu@outlook.com>
2020-03-03 06:24:36 -0800

    udpate

--- sender.py
+++ sender.py
@@ -1,14 +1,26 @@
 from scapy.all import *
 
-PCAPFILE=""
+PCAPFILE="ten.pcap"
+IFACE="p9p1"
+IDX=4
+
+#sendp(IP(ttl=10), iface=IFACE)
+
 
 f=rdpcap(PCAPFILE)
-idx = 1
+
+pkt = f[IDX]
+
+new_pkt = 
+
+
+#for pkt in f:
+#    pkt.show()
 
 cnt=100
 while cnt>0:
-    sendp(f[idx])
+    sendp(pkt, iface=IFACE)
     cnt-=1
 
-f[idx].show()
-f[idx].dumppdf("pkt_"+str(idx)+".pdf")
+#f[idx].show()
+#f[idx].dumppdf("pkt_"+str(idx)+".pdf")
